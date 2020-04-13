caf7c4e459a70cbacf8ea9af4a7102a5cc68d5aa
Dmitry <donveds@gmail.com>
2020-04-13 10:17:33 +0300

    Add posts

---
+++ my-app/src/components/main/Profile/Posts/Post/Post.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Post.css"
+
+const Post = (post) => {
+  return (
+    <div className="Profile-posts-post">
+      <p><b>{post.title || "Title"}</b></p>
+      <p>{post.text || "Text"}</p>
+
+  <p>Best regards, {"your dear friend"}</p>
+    </div>
+  );
+};
+
+export default Post; 
--- my-app/src/components/main/Profile/Posts/Posts.jsx
+++ my-app/src/components/main/Profile/Posts/Posts.jsx
@@ -1,9 +1,16 @@
 import React from "react";
+import Post from "./Post/Post"
+import "./Posts.css"
 
 const Posts = () => {
   return (
     <div className="Profile-posts">
       <h3>Posts:</h3>
+      <div className="Profile-posts_card">
+        <Post title="Greeting" text="Hello to all my friends" />
+        <Post title="Weather" text="Today is really cold" />
+        <Post />
+      </div>
     </div>
   );
 };

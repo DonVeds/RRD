859240f204d6eb6e9b677452954ff44b844024cd
Dmitry <donveds@gmail.com>
2020-10-12 22:54:35 +0300

    add some OOP to redux/state.js

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,56 +1,60 @@
 // import { renderEntireTree } from "../render";
+let store = {
 
-let renderEntireTree = () => {
-  console.log("ghghghghghgh")
-}
-
-let state = {
-  profilePageData: {
-    posts: [
-      { id: "1", title: "Hi", text: "Hello to all my friends", likesCount: "12" },
-      { id: "2", title: "Weather", text: "Today is really cold", likesCount: "11" },
-      { id: "3", title: "test post", text: "post", likesCount: "10" },
-      { id: "4", title: "test post", text: "post", likesCount: "10" },
-      { id: "5", title: "test post", text: "post", likesCount: "10" }
-    ],
-    newPostText: "Какой хороший пост"
+  _state: {
+    profilePageData: {
+      posts: [
+        { id: "1", title: "Hi", text: "Hello to all my friends", likesCount: "12" },
+        { id: "2", title: "Weather", text: "Today is really cold", likesCount: "11" },
+        { id: "3", title: "test post", text: "post", likesCount: "10" },
+        { id: "4", title: "test post", text: "post", likesCount: "10" },
+        { id: "5", title: "test post", text: "post", likesCount: "10" }
+      ],
+      newPostText: "Какой хороший пост"
+    },
+    messagesPageData: {
+      dialogs: [
+        { id: "1", name: "Misha" },
+        { id: "2", name: "Masha" },
+        { id: "3", name: "Mosha" },
+        { id: "4", name: "Musha" },
+        { id: "5", name: "Mesha" }
+      ],
+      messages: [
+        { id: "1", message: "Hi" },
+        { id: "2", message: "How are you?" },
+        { id: "3", message: "What a good day" },
+        { id: "4", message: "We are near shipyard" },
+        { id: "5", message: "Karamba" }
+      ]
+    }
   },
-  messagesPageData: {
-    dialogs: [
-      { id: "1", name: "Misha" },
-      { id: "2", name: "Masha" },
-      { id: "3", name: "Mosha" },
-      { id: "4", name: "Musha" },
-      { id: "5", name: "Mesha" }
-    ],
-    messages: [
-      { id: "1", message: "Hi" },
-      { id: "2", message: "How are you?" },
-      { id: "3", message: "What a good day" },
-      { id: "4", message: "We are near shipyard" },
-      { id: "5", message: "Karamba" }
-    ]
+  _callSubscriber() {
+    console.log("State changed");
+  },
+  addPost() {
+    let newPost = {
+      id: this._state.profilePageData.posts.length + 1, 
+      text: this._state.profilePageData.newPostText,
+      likesCount: 0
+    };
+    this._state.profilePageData.posts.push(newPost);
+    this._state.profilePageData.newPostText = "";
+    this._callSubscriber(this._state);
+  },
+  updateNewPostText(newText) {
+    this._state.profilePageData.newPostText = newText;
+    this._callSubscriber(this._state);
+  },
+  subscribe(observer) {
+    this._callSubscriber = observer;
+  },
+
+  getState() {
+    return this._state
   }
-};
 
-export let addPost = () => {
-  let newPost = {
-    id: state.profilePageData.posts.length + 1, 
-    text: state.profilePageData.newPostText,
-    likesCount: 0
-  };
-  state.profilePageData.posts.push(newPost);
-  state.profilePageData.newPostText = "";
-  renderEntireTree();
 }
 
-export let updateNewPostText = (newText) => {
-  state.profilePageData.newPostText = newText;
-  renderEntireTree();
-};
-
-export const subscribe = (observer) => {
-  renderEntireTree = observer;
-}
-
-export default state;
+export default store;
+Window.store = store;
